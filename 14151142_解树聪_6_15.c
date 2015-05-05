5375c2475042e23f52fc74a26acfc8427ac8c5f9
2015CLanguage <buaa_2015_c@163.com>
2015-05-05 14:32:50 +0800

    Create 14151142_解树聪_6_15.c

---
+++ 14151142_解树聪_6_15.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s1[80],s2[80];
+	int i;
+	printf("请输入字符 s2:");
+	gets(s2);
+	for(i=0;i<=strlen(s2);i++)
+		s1[i]=s2[i];
+	printf("s2为:");
+	puts(s1);
+	return 0;
+}
+	
