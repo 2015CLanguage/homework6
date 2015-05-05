f06347265576c6e2ee37094caaba61c8505a3e23
2015CLanguage <buaa_2015_c@163.com>
2015-05-05 14:01:56 +0800

    Create 14151133_武世杰_6_6.c

--- 14151133_武世杰_6_6.c
+++ 14151133_武世杰_6_6.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 void main()
 {
-	int i,j,a[11][11]={0};
-	for(i=0;i<=10;i++)
+	int i,j,a[10][10]={0};
+	for(i=0;i<=9;i++)
 	{
 		a[i][i]=1;
 		a[i][0]=1;
 	}
-	for(i=2;i<=10;i++)
+	for(i=2;i<=9;i++)
 		for(j=1;j<=i;j++)
 			a[i][j]=a[i-1][j]+a[i-1][j-1];
-	for(i=0;i<=10;i++)
+	for(i=0;i<=9;i++)
 	{
 		for(j=0;j<=i;j++)
 			printf("%4d",a[i][j]);
