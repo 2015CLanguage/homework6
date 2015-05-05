02e9a6568900fabaef80a3eb75b9b40437b61cfb
2015CLanguage <buaa_2015_c@163.com>
2015-05-05 14:59:46 +0800

    Create 14151166_于琦_6_15.c

---
+++ 14151166_于琦_6_15.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s1[80],s2[80];
+	int i;
+	printf("请输入s2中字符");
+	gets(s2);
+	for(i=0;i<=strlen(s2);i++)
+		s1[i]=s2[i];
+	puts(s1);
+	return 0;
+}
