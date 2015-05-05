c78ac7501d917b48f28af3094271ea3fe66e8f70
2015CLanguage <buaa_2015_c@163.com>
2015-05-05 15:16:54 +0800

    Create 14151165_王群_6_8.c

---
+++ 14151165_王群_6_8.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    int a[4][4],i,j,k,m,n,p=0;
+	for(i=0;i<4;i++){
+		for(j=0;j<4;j++){
+            printf("a[%d][%d]=?",i,j);
+			scanf("%d",&a[i][j]);
+		}
+	}
+	k=a[0][0];
+	for(i=0;i<4;i++){
+		for(j=0;j<4;j++){
+			if(a[i][j]>k){
+				k=a[i][j];
+				m=j;
+			}
+		}
+		for(n=0;n<4;n++){
+			if(a[n][m]<k){
+				p=1;
+				break;
+			}
+		}
+		if(p==0){
+			printf("a[%d][%d]=%d",i,m,k);
+			break;
+		}
+	}
+	if(p==1)
+		printf("无");
+	return 0;
+}
