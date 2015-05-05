57df3fdbf623388d83c04a915387208a5b2369b9
2015CLanguage <buaa_2015_c@163.com>
2015-05-05 15:21:12 +0800

    Update 14151184_张聪_6_15.c

--- 14151184_张聪_6_15.c
+++ 14151184_张聪_6_15.c
@@ -5,9 +5,9 @@ int main()
     char s1[80],s2[80];
     int i;
     printf("input s2:");
-    gets(s2);
+    scanf("%s",s2);
     for(i=0;i<=strlen(s2);i++)
         s1[i]=s2[i];
-    puts(s1);
+    printf("%s",s1);
     return 0;
 }
