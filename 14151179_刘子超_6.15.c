0625efdd1bf60467bfb88df2ca542aedaa186c1a
2015CLanguage <buaa_2015_c@163.com>
2015-05-07 16:09:05 +0800

    Create 14151179_刘子超_6.15.c

---
+++ 14151179_刘子超_6.15.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char s2[9],s1[9];
+	int i;
+	gets(s2);
+	for(i=0;i<9;i++)
+	{
+		if(s2[i]=='\0')
+		{
+			s1[i]='\0';
+			break;
+        }
+		else s1[i]=s2[i];
+	}
+	puts(s1);
+}
