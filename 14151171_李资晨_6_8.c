fed14dee2ae1ab8e69b5e2c512f437fc7b969ead
2015CLanguage <buaa_2015_c@163.com>
2015-05-08 02:37:02 +0800

    Create 14151171_李资晨_6_8.c

---
+++ 14151171_李资晨_6_8.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+int main()
+{
+	int a[4][4], i, j, num=0, k;
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 4; j++)
+			scanf("%d", &a[i][j]);
+	}
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			for (k = 0; k < 4; k++)
+			if (a[i][j] < a[i][k]) break;
+			if (k == 4)
+			{
+				for (k = 0; k < 4; k++)
+				if (a[i][j] > a[k][j]) break;
+				if (k == 4)
+				{
+					printf("鞍点(%d,%d),%d\n", i + 1, j + 1, a[i][j]);
+					num++;
+				}
+			}
+		}
+	}
+	if (num == 0) printf("无");
+	return 0;
+}
