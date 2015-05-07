dfff09c322e5ee5d58af246436f92d7479f87f73
2015CLanguage <buaa_2015_c@163.com>
2015-05-07 20:21:01 +0800

    Update 14151200_陈维正_6_10.c

--- 14151200_陈维正_6_10.c
+++ 14151200_陈维正_6_10.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
 void main()
 {
-char string[3][3];
+char string[3][80];
 char c;
-int i,j,letter=0,num=0;
+int i,j,a=0,b=0,d=0,e=0,f=0;
 
 for(i=0;i<3;i++)
 	gets(string[i]);
 
 for(i=0;i<3;i++)
 	{
-	for(j=0;(c=string[i][j])!='\0';j++)
+	for(j=0;j<3&&(c=string[i][j])!='\0';j++)
 		{if(c<=90&&c>=65)
-			num++;
+			a++;
+		else if(c<='z'&&c>='a')
+			b++;
+		else if(c<='9'&&c>='0')
+			d++;
+		else if(c==' ')
+			e++;
+		else
+			f++;
 		}
 	}
-printf("大写字母有:%d\n",num);
+
+printf("大写字母有:%d\n",a);
+printf("小写字母有:%d\n",b);
+printf("数字有:%d\n",d);
+printf("空格有:%d\n",e);
+printf("其他字符有:%d\n",f);
 }
