1a16d34f88d59af2b03caca36fa4e3cd8ae505e4
2015CLanguage <buaa_2015_c@163.com>
2015-05-07 21:57:04 +0800

    Create 14151127_唐敏佳_6_6.c

---
+++ 14151127_唐敏佳_6_6.c
@@ -0,0 +1,34 @@
+#include "stdio.h"
+
+int main()
+{
+    int s[10][10],i,j;
+    for(i=0;i<10;i++)
+    {
+        for(j=0;j<10;j++)
+        {
+            s[i][j]=0;
+        }
+    }
+    s[0][0]=1;
+    for(i=1;i<10;i++)
+    {
+        s[i][0]=1;
+        s[i][i]=1;
+        for(j=1;j<10;j++)
+        {
+            s[i][j]=s[i-1][j-1]+s[i-1][j];
+        }
+    }
+    for(i=0;i<10;i++)
+    {
+        for(j=0;j<10;j++)
+        {
+            if(s[i][j]!=0)
+            {
+                printf("%5d",s[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
