9fe4459328814cfc9b5b385d3572a0a636e97193
2015CLanguage <buaa_2015_c@163.com>
2015-05-10 12:40:38 +0800

    Create 14151187_孙衡_6_10.c

--- 14151187_孙衡_6_10.c
+++ 14151187_孙衡_6_10.c
@@ -1,23 +1,32 @@
-#include"stdio.h" 
-int main() 
-{ 
- 	int a[10][10],i,j; 
- 	for(i=0;i<10;i++) 
-	{ 
-		a[i][i]=1; 
-		a[i][0]=1; 
-	} 
- 	for(i=2;i<10;i++) 
- 	{ 
- 		for(j=1;j<i;j++) 
- 			a[i][j]=a[i-1][j-1]+a[i-1][j]; 
- 	} 
- 	for(i=0;i<10;i++) 
- 	{ 
- 		for(j=0;j<=i;j++) 
- 			printf("%-5d",a[i][j]); 
- 		printf("\n"); 
- 	} 
- 	printf("\n"); 
- 	return 0; 
-} 
+#include"stdio.h"
+int main()
+{
+	char a[3][80];
+	int d,x,k,s,q,i,j;
+	printf("请输入三行文字:\n");
+	for(i=0;i<3;i++)
+	gets(a[i]);
+	d=x=s=q=k=0;
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<80&&a[i][j]!='\0';j++)
+		{
+			if(a[i][j]>='A'&&a[i][j]<='Z')
+			   d++;
+			else if(a[i][j]>='a'&&a[i][j]<='z')
+			   x++;
+			else if(a[i][j]==' ')
+			   k++;
+			else if(a[i][j]>='0'&&a[i][j]<='9')
+			   s++;
+			else
+			   q++; 
+		}
+	}
+	    printf("大写字母个数为:%d\n",d);
+	    printf("小写字母个数为:%d\n",x);
+	    printf("数字个数为:%d\n",s);
+	    printf("空格个数为:%d\n",k);
+	    printf("其他字符个数为:%d\n",q);
+    return 0;		
+}
